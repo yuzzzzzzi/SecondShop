d26b8c366a7024c91a8946622ab99aed6c1c705a
yuzzzzzzi <2583239879@qq.com>
2019-06-20 16:58:41 +0800

    Version:1.2

--- SecondShop(sqlserver)/store/urls.py
+++ SecondShop(sqlserver)/store/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path,re_path
-from . import views,viewsNativeSQL
+#from . import views
+from . import viewsNativeSQL as views
 
 app_name = 'store'
 
 urlpatterns = [
-    path('',viewsNativeSQL.index,name='index'),
-    path('search/',viewsNativeSQL.search,name='search'),
-    re_path(r'mysale/(?P<goods_id>\d+)?',viewsNativeSQL.mysale,name='mysale'),
-    path('add_goods/',viewsNativeSQL.add_goods,name='add_goods'),
-    re_path(r'details/(?P<goods_id>\d+)',viewsNativeSQL.details,name='details'),
-    path('addCheck/',viewsNativeSQL.addCheck,name='addCheck'),
-    re_path(r'shoppingcart/(?P<cart_id>\d+)?',viewsNativeSQL.shoppingcart,name='shoppingcart'),
-    path('orderCheck/',viewsNativeSQL.orderCheck,name='orderCheck'),
-    re_path(r'purchased/(?P<order_id>\d+)?',viewsNativeSQL.purchased,name='purchased'),
-    re_path(r'shop/(?P<type_id>\d+)?',viewsNativeSQL.shop,name='shop'),
+    path('',views.index,name='index'),
+    path('search/',views.search,name='search'),
+    re_path(r'mysale/(?P<goods_id>\d+)?',views.mysale,name='mysale'),
+    path('add_goods/',views.add_goods,name='add_goods'),
+    re_path(r'details/(?P<goods_id>\d+)',views.details,name='details'),
+    path('addCheck/',views.addCheck,name='addCheck'),
+    re_path(r'shoppingcart/(?P<cart_id>\d+)?',views.shoppingcart,name='shoppingcart'),
+    path('orderCheck/',views.orderCheck,name='orderCheck'),
+    re_path(r'purchased/(?P<order_id>\d+)?',views.purchased,name='purchased'),
+    re_path(r'shop/(?P<type_id>\d+)?',views.shop,name='shop'),
 ]
--- SecondShop(sqlserver)/user/urls.py
+++ SecondShop(sqlserver)/user/urls.py
@@ -1,15 +1,17 @@
 """为应用程序users定义URL模式"""
 
 from django.urls import path,re_path
-from . import views,viewsNativeSQL
+#from . import views
+from . import viewsNativeSQL as views
+
 
 app_name = 'user'
 
 urlpatterns = [
 	#登录界面
-	path('login/',viewsNativeSQL.ulogin,name='ulogin'),
+	path('login/',views.ulogin,name='ulogin'),
 	#注销
-	path('logout/',viewsNativeSQL.ulogout,name='ulogout'),
-	path('register/',viewsNativeSQL.register,name='register'),
-	re_path(r'person/(?P<user_id>\d+)',viewsNativeSQL.person,name = 'person'),
+	path('logout/',views.ulogout,name='ulogout'),
+	path('register/',views.register,name='register'),
+	re_path(r'person/(?P<user_id>\d+)',views.person,name = 'person'),
 ]
